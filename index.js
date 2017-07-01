235537a89aa24f4542eb876d8706079d1690a503
lBeJIuk <sergeyzarva@gmail.com>
2017-07-01 20:51:41 +0200

    Init

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+const fs = require('fs');
+
+ module.exports = (fileName = 'timeManager.json') => {
+   return {
+     'init':function(){ // функция инициализации.   Принимает имя файла в который делать записи или использует стандартный
+         if (!fs.existsSync(process.cwd() + '/' + fileName) ){// проверка есть ли файл
+           let initData = {}; // переменная для создания новго файла
+           let date = new Date();
+           initData.HstartDate = date.getDate()+'-' + date.getMonth()+'-' +date.getFullYear() +' '+date.getHours() + ':' + date.getMinutes();// дата начала разработки в удобочитаемом виде
+           initData.UstartDate = Date.now();// дата начала разработки в формате UNIX
+           initData.startCount = 0;//   количество запусков разработки
+           initData.UpureTimeSpent = 0;// чистое затраченое время в формате UNIX
+           initData.HpureTimeSpent = 0;// чистое затраченое время в удобочитаемом виде
+           initData.allTimeSpent = 0;// "грязное" затраченое время(сколько в общем дней прошло)
+           fs.writeFileSync(process.cwd() + '/' + fileName, JSON.stringify(initData));
+         }
+        //инициализация нового запуска разработки
+        let CurrentData = Date.now();
+        let readData = fs.readFileSync(fileName);
+        const DAYAGO = 86400000; //дней прошло
+        const MINAGO = 60000; //минут прошло
+        readData = JSON.parse(readData.toString());//получаем нормальный обьект
+        readData.startCount++; //добавляем одно включение разработки
+        readData.currentStartDev = CurrentData; // сделано для подсчета затраченого времени за одно включение
+        readData.allTimeSpent = Math.floor((CurrentData - readData.UstartDate)/DAYAGO) ; // изменяем общее время работы над проектом
+        readData.HpureTimeSpent = Math.floor(readData.UpureTimeSpent / MINAGO);// изменяем общее время работы над проектом
+        fs.writeFileSync(process.cwd() + '/' + fileName, JSON.stringify(readData));
+        // 33 - name yellow
+        // 34 - value blue
+        //разноцветный вывод информации
+          console.log('\x1b[33m', 'Development is started' ,'\x1b[0m');
+          console.log('\x1b[33m', 'It is spent on development precisely: ' ,'\x1b[34m', readData.HpureTimeSpent+ ' minutes','\x1b[0m');
+          console.log('\x1b[33m', 'Total starts: ' ,'\x1b[34m', readData.startCount + ' time','\x1b[0m');
+          console.log('\x1b[33m', 'Spent on development in general: ' ,'\x1b[34m', readData.allTimeSpent + ' day','\x1b[0m');
+          console.log('\x1b[33m', 'Development was started: ' ,'\x1b[34m', readData.HstartDate ,'\x1b[0m');
+     },
+      'count' : function() { // функция для изменения текущего затраченого время
+        let readData = fs.readFileSync(fileName);
+        readData = JSON.parse(readData.toString());//получаем нормальный обьект
+        readData.UpureTimeSpent = readData.UpureTimeSpent + Date.now() - readData.currentStartDev; // время что было плюс время что прошло с момента инициализации до сейчас
+        fs.writeFileSync(process.cwd() + '/' + fileName, JSON.stringify(readData));
+      }
+   }
+ }
