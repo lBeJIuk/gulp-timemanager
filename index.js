495ef3c49273cf1a6ef994ac2073d17e5cc3049d
lBeJIuk <sergeyzarva@gmail.com>
2017-07-04 21:11:11 +0200

    Исправил ошибку с подсчетом времени

--- index.js
+++ index.js
@@ -22,23 +22,27 @@ const fs = require('fs');
         const MINAGO = 60000; //минут прошло
         readData = JSON.parse(readData.toString());//получаем нормальный обьект
         readData.startCount++; //добавляем одно включение разработки
-        readData.currentStartDev = CurrentData; // сделано для подсчета затраченого времени за одно включение
         readData.allTimeSpent = Math.floor((CurrentData - readData.UstartDate)/DAYAGO) ; // изменяем общее время работы над проектом
+        readData.UpureTimeSpent +=  readData.currentDurationDev;
         readData.HpureTimeSpent = Math.floor(readData.UpureTimeSpent / MINAGO);// изменяем общее время работы над проектом
+        readData.currentStartDev = CurrentData; // сделано для подсчета затраченого времени за одно включение
+        readData.currentDurationDev = 0; // сколько длилась сесия , для подсчета время разработки
         fs.writeFileSync(process.cwd() + '/' + fileName, JSON.stringify(readData));
         // 33 - name yellow
         // 34 - value blue
         //разноцветный вывод информации
+          console.log();
           console.log('\x1b[33m', 'Development is started' ,'\x1b[0m');
           console.log('\x1b[33m', 'It is spent on development precisely: ' ,'\x1b[34m', readData.HpureTimeSpent+ ' minutes','\x1b[0m');
           console.log('\x1b[33m', 'Total starts: ' ,'\x1b[34m', readData.startCount + ' time','\x1b[0m');
           console.log('\x1b[33m', 'Spent on development in general: ' ,'\x1b[34m', readData.allTimeSpent + ' day','\x1b[0m');
           console.log('\x1b[33m', 'Development was started: ' ,'\x1b[34m', readData.HstartDate ,'\x1b[0m');
+          console.log();
      },
       'count' : function() { // функция для изменения текущего затраченого время
         let readData = fs.readFileSync(fileName);
         readData = JSON.parse(readData.toString());//получаем нормальный обьект
-        readData.UpureTimeSpent = readData.UpureTimeSpent + Date.now() - readData.currentStartDev; // время что было плюс время что прошло с момента инициализации до сейчас
+        readData.currentDurationDev = Date.now() - readData.currentStartDev; // время что прошло с момента инициализации до сейчас
         fs.writeFileSync(process.cwd() + '/' + fileName, JSON.stringify(readData));
       }
    }
